dd13fd7dde98f4420cc9430316d8f5bd39b647da
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2019-12-02 09:38:22 -0800

    added code to follow player script

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    
+        // Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +17,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
