593bcf6510f685c09ff8ac65a2bf35ce3b4e41f8
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2019-12-12 10:16:00 -0800

    added new code

---
+++ Assets/scripts/DetectCollisions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+     }
+  private void OnTriggerEnter(Collider other)
+  {
+   Destroy(gameObject);
+   Destroy(other.gameObject);
+  }
+    }
+                
+
