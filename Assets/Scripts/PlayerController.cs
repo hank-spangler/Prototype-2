3699686cab4636d8d2b35a428ee036db0954172a
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2020-03-02 12:30:20 -0800

    changed player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,29 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-    [SerializeField] string axisName = "Set Input Axis";
-    [SerializeField] float speed = 10.0f;
-    [SerializeField] float xBound = 12.0f;
+public class PlayerController : MonoBehaviour
+{
+   public string axisName = "Set Input Axis";
+    public float speed = 10.0f;
+    public float xBound = 12.0f;
+    public float zBound = 12.0f;
+    public string AxisName = "Set Input Axis";
 
     // Start is called before the first frame update
-    void Start () {
+    void Start () 
+    {
 
     }
 
-    [SerializeField] float horizontalInput;
+    private float horizontalInput;
+    private float verticalInput;
 
     // Update is called once per frame
-    void Update () {
+    void Update () 
+    {
         // Player movement.
         horizontalInput = Input.GetAxis (axisName);
         transform.Translate (horizontalInput * speed * Time.deltaTime * Vector3.right);
-
-        if (transform.position.x < -xBound) {
+        
+        verticalInput = Input.GetAxis (AxisName);
+        transform.Translate (verticalInput * speed * Time.deltaTime * Vector3.forward);
+        
+        // X axis bounds
+        if (transform.position.x < -xBound) 
+        {
             transform.position = new Vector3 (-xBound, transform.position.y, transform.position.z);
         }
-        if (transform.position.x > xBound) {
+        if (transform.position.x > xBound) 
+        {
             transform.position = new Vector3 (xBound, transform.position.y, transform.position.z);
+        }
+        // Z axis bounds
+        if (transform.position.z < -zBound) 
+        {
+            transform.position = new Vector3 (-zBound, transform.position.y, transform.position.x);
+        }
+        if (transform.position.z > zBound) 
+        {
+            transform.position = new Vector3 (zBound, transform.position.y, transform.position.x);
         }
     }
 }
