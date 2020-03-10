b498e7f2c61756f0d6f03265444b1e57386eec65
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2020-03-10 12:02:46 -0700

    SpawnMangaer

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
     }
 }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    public float spawnRangeX = 12.0f;
+    public float spawnPosz = 12.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
