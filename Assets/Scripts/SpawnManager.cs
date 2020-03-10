405ebc287b035d155ad8329d40db4f1d374ea4e2
hank-spangler <56128553+hank-spangler@users.noreply.github.com>
2020-03-10 12:27:45 -0700

    more spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs;
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    public float spawnRange = 12.0f;
+    public int enemyAmount = 3;
 
-    public float spawnRangeX = 12.0f;
-    public float spawnPosz = 12.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemy(enemyAmount);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount < 3)
+        {
+            SpawnEnemy(enemyAmount);
+        }
     }
+    
+    //Spawn Position
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosz = Random.Range(-spawnRange, spawnRange);
+        
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosz);
+        return randomPos;
+    } 
 }
+
